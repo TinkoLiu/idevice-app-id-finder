92e1586c5690581806d4f6415e10690db42ca991
Tinko Liu <TinkoLiu@users.noreply.github.com>
2024-02-13 16:05:47 +0800

    chore: bump version

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,7 @@ const program = new Command()
 
 program
   .name('idevice-app-id-finder')
-  .version('1.0.1')
+  .version('1.1.0')
   .description('A tool for figuring out what Apple ID was used for each apps on an iOS device')
 
 registerCommandDevices(program)
